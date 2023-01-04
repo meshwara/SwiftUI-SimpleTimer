2766c3f1152d3d6326df5a79711c2ff343ca00ef
D. Prameswara <meshwara@gmail.com>
2023-01-05 05:59:06 +0700

    Initial Commit

---
+++ DemoStopWatch/DemoStopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoStopWatchApp.swift
+//  DemoStopWatch
+//
+//  Created by D. Prameswara on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoStopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
